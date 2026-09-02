e3a8af8bea76a36d98ef081461d4171455dc535f
parsa4418 <parsayt2024@gmail.com>
2026-09-02 14:47:51 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,8 +1,15 @@
+from .library import *
 import os
 
 admin_user_id = int(os.environ['ADMIN_USER_ID'])
 api_id = int(os.environ['API_ID'])
 api_hash = os.environ['API_HASH']
+helper_username = os.environ['HELPER_USERNAME']
 bot_token = os.environ['BOT_TOKEN']
+
 client_id = os.environ['SPOTIFY_CLIENT_ID']
 client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+
+client = TelegramClient('TRself-MT', api_id, api_hash)
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
