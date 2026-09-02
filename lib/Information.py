c56f448563862e2121ded639cc3d695523f1c590
parsa4418 <parsayt2024@gmail.com>
2026-09-02 14:41:52 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,14 +1,8 @@
-from .library import *
+import os
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
-
-client_id = '01e7dc6b41c3471b94efe87abeb05919'
-client_secret = '4f5f93af1ced4b0d9ba8440606803639'
-
-client = TelegramClient('TRself-MT', api_id, api_hash)
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+admin_user_id = int(os.environ['ADMIN_USER_ID'])
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
+bot_token = os.environ['BOT_TOKEN']
+client_id = os.environ['SPOTIFY_CLIENT_ID']
+client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
